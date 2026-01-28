509005a5d6707b10c84294101a33a3465f6c8966
ohjh9701 <ohjh4875@gmail.com>
2026-01-28 14:45:52 +0900

    [생성] Customer 실습을 위한 스프링부트 생성

---
+++ CustomerCRUDTesting/src/main/java/com/customer/CustomerCrudTestingApplication.java
@@ -0,0 +1,13 @@
+package com.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCrudTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCrudTestingApplication.class, args);
+	}
+
+}
